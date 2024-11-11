26e77322c3dce42f40fa63746577de929a56b0e7
Felipe Gabriel <68780083+Flipe-TI@users.noreply.github.com>
2024-11-10 23:33:13 -0300

    adjust cases with no asks for secundary project

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ async def ask(data_input: DataInput):
         # Criar DataFrame a partir do JSON
         df = pd.DataFrame(data_input.data)
 
-        system = "Você é um assistente e essa é a resposta que deve passar para o usuario, seja bem didatico e criativo"
+        system = "Você é um assistente e essa é a resposta que deve passar para o usuario, seja bem didatico e criativo. Caso não tenha nenhuma pergunta informe para o usuario fazer essa pergunta no sistema, para que assim possamos melhorar o procedimento e caso ainda não tenha resposta mas a pergunta já foi cadastrada fale que estamos correndo para buscar a resposta."
         human = "{text}"
         prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
         chain = prompt | fiscal_de_llm
